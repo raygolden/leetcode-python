ef82511addca9c54b08288f46d431201aed9e4f7
Yan Huang <yan@eyesee360.com>
2013-11-08 20:59:55 -0500

    powerset without duplicates

---
+++ src/powerset.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+""" Powerset """
+
+def powerset(s):
+    result = []
+    
+    result.append([])
+    
+    for e in s:
+        l = len(result)
+        i = 0
+        while i < l:
+            res = list(result[i])
+            res.append(e)
+                        
+            result.append(res)
+            i += 1
+        
+        
+    return result
+
+
+if __name__ == '__main__':
+   print powerset([1, 2, 3])
