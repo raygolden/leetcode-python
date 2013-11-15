4e844cf3f9ede68e540ed93f5b30c37dfc2a58f1
Yan Huang <yan@eyesee360.com>
2013-11-14 22:05:43 -0500

    finished regex

--- src/regex.py
+++ src/regex.py
@@ -12,26 +12,41 @@ The function prototype should be:
 bool isMatch(const char *s, const char *p)
 
 Some examples:
-isMatch("aa","a") → false
-isMatch("aa","aa") → true
-isMatch("aaa","aa") → false
-isMatch("aa", "a*") → true
-isMatch("aa", ".*") → true
-isMatch("ab", ".*") → true
-isMatch("aab", "c*a*b") → true
+isMatch("aa","a")  false
+isMatch("aa","aa")  true
+isMatch("aaa","aa")  false
+isMatch("aa", "a*") true
+isMatch("aa", ".*")  true
+isMatch("ab", ".*")  true
+isMatch("aab", "c*a*b")  true
 """
 
-def isMatch(p, s):
+def isMatch(s, p):
     if p and s:
-        if p[0] == s[0]:
-            return isMatch(p[1:], s[1:])
-        elif p[0] == '.':
-            if len(s) > 2 and s[1] == '*':
-                return isMatch(p[1:], s[2:])
-    else:
-        if not p and not s:
-            return True
+        if p[0] == s[0] or p[0] == ".":
+            if len(p) > 1 and p[1] == '*':
+                return isMatch(s[1:], p)
+            return isMatch(s[1:], p[1:])
+        else:
+            if len(p) > 1 and p[1] == '*':
+                return isMatch(s, p[2:])                
+                
+    elif not p and not s:
+        return True
+        
+    elif p and not s:
+        if len(p) == 2 and p[1] == '*': return True
 
 
     return False
 
+
+
+print isMatch("aa","a")
+print isMatch("aa","aa")
+print isMatch("aaa","aa")
+print isMatch("aa", "a*")
+print isMatch("aa", ".*")
+print isMatch("ab", ".*")
+print isMatch("aab", "c*a*b")
+print isMatch('abcde', ".b*de")
